076270ffe2260ce81391781e27c176b92b03e311
kyunghee kwak <kwakkyunghee325@gmail.com>
2023-05-25 09:26:40 +0900

    속성(property) 실습

--- ClassAdv/Program.cs
+++ ClassAdv/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +12,27 @@ namespace ClassAdv
     {
         static void Main(string[] args)
         {
-            //소멸자 예시
-            Product product1 = new Product("과자", 1000);
-            Product product2 = new Product("아이스크림", 2000);
-            Product product3 = new Product("케이크", 3000);
+
+//소멸자 예시
+Product product1 = new Product("과자", 1000);
+
+Product product2 = new Product("아이스크림", 2000);
+Product product3 = new Product("케이크", 3000);
+
+//속성 (property) 실습
+Box box = new Box(10, 10);
+box.Width = 100;
+box.Height = 100;
+box.GetArea();
+
+try
+{
+box.Width = -10;
+}catch (Exception ex) 
+{ 
+Console.WriteLine(ex.Message);
+}
+
         }
     }
 }
